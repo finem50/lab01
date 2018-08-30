e20d8929444a964d6f1fdf5f2cb8c06ea4a9b611
Bruce <finem@live.com>
2018-08-30 11:52:47 -0500

    Fixing file extension and pushing compiled code

--- p11.cpp
+++ p11.cpp
@@ -10,19 +10,20 @@ int main( )
      double profit_per_bottle = 0.2;  // 20 cents per bottle profit
      double profit_per_day, profit_per_year;
 
-    cout << “Press enter after entering each number \n”;
-    cout << “Enter number of cases \n”;
+    cout << "Press enter after entering each number \n";
+    cout << "Enter number of cases \n";
+
     cin >> cases_per_day;
 
     profit_per_day = cases_per_day * bottles_per_case * profit_per_bottle;
     profit_per_year = 365 * profit_per_day;
 
-    cout << “The store has a made : “;
+    cout << "The store has a made : ";
     cout << profit_per_day;
-    cout << “  per day. \n”;
-    cout << “That means the profit for one year will be:  “;
+    cout << "  per day. \n";
+    cout << "That means the profit for one year will be:  ";
     cout << profit_per_year << endl;
+    cout << "Good business?! \n";
 
-    cout << “Good business?! \n”;
     return 0;
-} 
+}
